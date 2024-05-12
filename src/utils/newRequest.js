4d54faac91a49631630460c7be7e91f988704016
muhammad-usama-anjum <usamaanjum67@gmail.com>
2024-05-12 17:31:45 +0500

    Added backend link

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest =axios.create({
-    baseURL:"https://freelancer-hub-backend.vercel.app/api/",
+    baseURL:"https://freelancerhub-backend-2.onrender.com/api/",
     withCredentials:true,
 })
 
